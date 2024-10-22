ec80ce4764337fa37b1f8ba0ffc9b3e236c0dfee
TonyayaDev <tonyayadev@gmail.com>
2024-10-22 19:29:50 +0100

    00-Kotlin

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/kotlin/tonyayadev.kt
@@ -0,0 +1,28 @@
+// Tipos de datos
+
+// Texto
+var caracter: Char = "e"
+var cadenaTexto: String = "Una cadena de texto"
+
+// Numeros
+var numero: Byte = 4
+var corto: Short = 25
+var entero: Int = 3586587
+var largo: Long = 7L
+var decimal: Double = 35.26
+var flotante: Float = 25.365f
+
+// Buleanos
+var buleano: Boolean = true
+
+// Arrays
+var arrayNumerico: Array<Int> = arrayOf(5,8,6,3,4,5,7,8,8)
+
+// No negativos
+var noNegnumero: UByte = 5U
+var noNegCorto: UShort = 58U
+var noNegEntero: UInt = 58U
+var noNegLargo: ULong = 58U
+
+// Imprimir por pantalla un mensaje
+print("Hola, Kotlin")
