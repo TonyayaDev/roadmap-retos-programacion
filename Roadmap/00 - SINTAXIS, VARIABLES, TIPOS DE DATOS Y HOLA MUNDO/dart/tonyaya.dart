076c1904aab039459ca2ba8cc4358fbc48d01525
TonyayaDev <tonyayadev@gmail.com>
2024-10-23 17:29:02 +0100

    #00-Dart

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/dart/tonyaya.dart
@@ -0,0 +1,26 @@
+// Url del lenguaje: https://dart.dev//
+
+// Comentario de una linea
+
+/* Comentario de varias
+  lineas a la vez 
+*/
+// Variables y Costantes
+var variable = "Soy una variable";
+const constante = "Soy una constante";
+
+// Tipos de Variables
+String textoGen = "Una cadena de texto";
+int numeroGen = 23;
+double decimalGen = 23.5;
+bool buleanoGen = true;
+List<int> lista = <int>[1, 5, 6, 9];
+List<String> listaString = <String>["hola", "Adios", "Mañana"];
+List<Object> listaObject = <Object>[3, "Lunes", 5.98, true];
+Set<String> setUnicos = <String>{'uno', 'dos', 'tres', 'cuatro'};
+Map<int, String> mapKey = <int, String>{1: 'Lunes', 2: 'Martes'};
+
+void main() {
+  // Imprimimoshola Dart
+  print("¡Hola Dart!");
+}
